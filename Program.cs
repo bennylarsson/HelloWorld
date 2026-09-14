cb91e4f4841ec1a3edfdaa9da3faa9e6e8175286
bennylarsson <benny.larsson@hotmail.com>
2026-09-14 23:09:03 +0200

    Update Program.cs

--- Program.cs
+++ Program.cs
@@ -1,1 +1,11 @@
-﻿Console.WriteLine("Hello, World!");
+﻿Console.Write(@"17774 PLANSLIP 0S 200
+       1 * 299.00                 299.00
+177363 SLIPPAPPER. 10 HÅL.K120.8 ST.
+       1 * 46.90                   46.90
+360599 ALUMINIUMNÄT. 250 X 200 MM
+       1 * 49.90                   49.90
+79449 VÄRMESKÖLD
+       1 * 199.00                 199.00
+----------------------------------------
+Totalt(SEK):                     594.80
+Tillbaka: 0.00");
