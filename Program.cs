511e90e6b4505300f7379a36546ab7a6b1d7384c
bennylarsson <benny.larsson@hotmail.com>
2026-09-15 08:56:41 +0200

    Update Program.cs

--- Program.cs
+++ Program.cs
@@ -1,4 +1,6 @@
-﻿for (int i = 1; i <= 10; i++)
+﻿Console.Write("Siffra : ");
+int siffra = int.Parse(Console.ReadLine());
+for (int i = 1; i <= 10; i++)
 {
-    Console.WriteLine($"Rad: {i}");
+    Console.WriteLine($"{i} * {siffra} = {i * siffra}");
 }
