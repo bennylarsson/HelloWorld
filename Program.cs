3f06599b607265e03c2935b2fdc34f8ea5800c33
bennylarsson <benny.larsson@hotmail.com>
2026-09-15 09:37:13 +0200

    Update Program.cs
    Växande fyrkanter klar!

--- Program.cs
+++ Program.cs
@@ -1,10 +1,15 @@
-﻿Console.Write("Storlek: ");
+﻿Console.WriteLine("Storlek: ");
 int storlek = int.Parse(Console.ReadLine());
-for (int i = 0; i < storlek; i++)
+
+for (int nuvarandestorlek = 1; nuvarandestorlek <= storlek; nuvarandestorlek++)
 {
-    for (int j = 0; j < storlek; j++)
+    for (int i = 0; i < nuvarandestorlek; i++)
     {
-        Console.Write("*");
+        for (int j = 0; j < nuvarandestorlek; j++)
+        {
+            Console.Write("*");
+        }
+        Console.WriteLine();
     }
     Console.WriteLine();
 }
