fc524830fdb54f9bedf18a6ba3e60b97c25493fd
bennylarsson <benny.larsson@hotmail.com>
2026-09-15 21:37:01 +0200

    Jämt och ojämt

--- Program.cs
+++ Program.cs
@@ -4,19 +4,20 @@ class Program
 {
     static void Main()
     {
-        Console.Write("Modulus: ");
-        int modulus = int.Parse(Console.ReadLine());
+        // Ber användaren om ett nummer
+        Console.Write("Nummer: ");
 
-        Console.Write("Från: ");
-        int franTal = int.Parse(Console.ReadLine());
+        // Läser in svaret som en sträng och omvandlar (parsar) det till ett heltal
+        int nummer = int.Parse(Console.ReadLine());
 
-        Console.Write("Till: ");
-        int tillTal = int.Parse(Console.ReadLine());
-
-        for (int i = franTal; i <= tillTal; i++)
+        // Kontrollerar om tal2et är jämnt eller ojämnt med hjälp av modulo (%)
+        if (nummer % 2 == 0)
         {
-            int resultat = i % modulus;
-            Console.WriteLine($"{i} % {modulus} = {resultat}");
+            Console.WriteLine($"{nummer} är jämnt");
+        }
+        else
+        {
+            Console.WriteLine($"{nummer} är ojämnt");
         }
     }
 }
