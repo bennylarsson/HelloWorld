a9221c1c209dfcf5e948628b1400758a67abd84f
bennylarsson <benny.larsson@hotmail.com>
2026-09-15 08:32:02 +0200

    Update Program.cs

--- Program.cs
+++ Program.cs
@@ -1,3 +1,4 @@
-﻿Console.Write("Första: "); int tal1 = int.Parse(Console.ReadLine());
-Console.Write("Andra: "); int tal2 = int.Parse(Console.ReadLine());
-Console.WriteLine("Resultat: " + (tal1 + tal2));
+﻿for (int i = 1; i <= 10; i++)
+{
+    Console.WriteLine($"Rad: {i}");
+}
