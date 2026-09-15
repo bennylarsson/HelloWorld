a804aa452cc63116e060f0ea25710d9e1bd2ee76
bennylarsson <benny.larsson@hotmail.com>
2026-09-15 20:42:07 +0200

    Klar med version 2

--- Program.cs
+++ Program.cs
@@ -1,15 +1,16 @@
 ﻿using System;
 
 class Program
-
 {
     static void Main()
     {
+        Console.Write("Modulus: ");
+        int modulus = int.Parse(Console.ReadLine());
+
         for (int i = 0; i <= 20; i++)
         {
-int resultat = i % 5;
-
-Console.WriteLine($"{i} % 5 = {resultat}");
-}
-}
-}
+            int result = i % modulus;
+            Console.WriteLine($"{i} % {modulus} = {result}");
+        }
+    }
+}   
